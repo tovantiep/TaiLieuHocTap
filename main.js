72a64b122590bc2bf72a95dfa4862a927704e84f
To Van Tiep <tovantiep2604@gmail.com>
2021-09-08 15:15:20 +0700

    project jquery

--- main.js
+++ main.js
@@ -6,70 +6,152 @@ const signupLink = document.querySelector("form .signup-link a");
 signupBtn.onclick = (() => {
     loginForm.style.marginLeft = "-50%";
     loginText.style.marginLeft = "-50%";
+    $('#error-message').css('display', 'none');
+    $('#success-message').css('display', 'none');
 });
 loginBtn.onclick = (() => {
     loginForm.style.marginLeft = "0%";
     loginText.style.marginLeft = "0%";
+    $('#error-message').css('display', 'none');
+    $('#success-message').css('display', 'none');
 });
 signupLink.onclick = (() => {
     signupBtn.click();
     return false;
 });
 var objPeople = [{
-
-    username: "tiep",
-    password: "1"
+    username: "tovantiep",
+    password: "123456"
 }]
-var i = 0;
-// var success = -1;
-
-function getInfo() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    for (var i = 0; i < objPeople.length; i++) {
-        if (username == objPeople[i].username && password == objPeople[i].password) {
-            document.getElementById("infor").innerHTML = "wellcome - " + username;
-            document.getElementById("wrapper").style.display = 'none';
-            document.getElementById("infor").style.display = 'block';
-            document.getElementById("logout").style.display = 'block';
-            return;
-        } else if (username == "" || password == "") {
-            alert("Khong duoc de trong tai khoan mat khau nha babe!");
-        } else {
-            alert("Sai tai khoan hoac mat khau roi babee!");
+$(document).ready(function() {
+    $('#login_btn').click(function() {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var success = -1;
+        $("#frmlogin").submit(function(event) {
+            event.preventDefault();
+        });
+        for (i = 0; i < objPeople.length; i++) {
+            if (username == objPeople[i].username && password == objPeople[i].password) {
+                $('#wrapper').css('display', 'none');
+                $('#infor').css('display', 'block');
+                $('#logout').css('display', 'block');
+                $('#inforname').html('welcome -' + username);
+                success = [i];
+            } else if (username == '' || password == '') {
+                //
+            } else {
+                myText = $("#error-message").text("Tài khoản hoặc mật khẩu không đúng");
+                $('#error-message').css('display', 'block');
+                $('#success-message').css('display', 'none');
+            }
         }
-    }
-}
+    });
+    $('#register_btn').click(function() {
+        var newUsername = $('#newUsername').val();
+        var newPassword = $('#newPassword').val();
+        var Repassword = $('#Repassword').val();
+        $("#frmRegister").submit(function(event) {
+            event.preventDefault();
+        });
+        for (i = 0; i < objPeople.length; i++) {
+            if (newUsername == '' || newPassword == '' || newEmail == '' || Repassword == '') {
+                console.log("Không được để trống");
+            } else if (newUsername == objPeople[i].username) {
+                myText = $("#error-message").text("Tài khoản đã tồn tại");
+                $('#error-message').css('display', 'block');
+                $('#success-message').css('display', 'none');
+                return;
+            } else if (Repassword != newPassword) {
+                console.log("Mật khẩu không trùng khớp");
+                myText = $("#error-message").text("Mật khẩu không trùng khớp");
+                $('#error-message').css('display', 'block');
+                $('#success-message').css('display', 'none');
+            } else {
+                var newUser = {
+                    username: newUsername,
+                    password: newPassword
+                }
+                objPeople.push(newUser);
+                console.log(objPeople);
+                myText = $("#success-message").text("Đăng kí thành công");
+                $('#error-message').css('display', 'none');
+                $('#success-message').css('display', 'block');
+                return;
+            }
 
-function logout() {
-    document.getElementById("wrapper").style.display = 'block';
-    document.getElementById("infor").style.display = 'none';
-    document.getElementById("logout").style.display = 'none';
-    document.getElementById("inforname").style.display = 'none';
-    // document.getElementsById("").reset();
-
-}
-
-function registerUser() {
-    var registerUser = document.getElementById("newUsername").value;
-    var registerPassword = document.getElementById("newPassword").value;
-    var registerRePassword = document.getElementById("rePassword").value
-    var newUser = {
-        username: registerUser,
-        password: registerPassword
-    }
-    for (i = 0; i < objPeople.length; i++) {
-        if (registerUser == objPeople[i].username) {
-            alert("Tai khan nay da ton tai");
-            return;
-        } else if (registerPassword.length < 5 || registerRePassword != registerPassword) {
-            alert("Mat khau khong du 5 ki tu hoac khong trung khop");
-        } else {
-            alert("Dang Ki thanh cong");
         }
-
-    }
-    console.log(objPeople);
-    objPeople.push(newUser)
-
-}
+    });
+    $('#logout').click(function() {
+        $('#wrapper').css('display', 'block');
+        $('#infor').css('display', 'none');
+        $('#logout').css('display', 'none');
+        $('#inforname').css('display', 'none');
+        $('#form-inner').trigger(reset);
+    });
+    $("#frmlogin").validate({
+        rules: {
+            username: {
+                required: true,
+                maxlength: 15,
+                minlength: 5
+            },
+            password: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            username: {
+                required: "Bắt buộc nhập username",
+                maxlength: "Hãy nhập tối đa 15 ký tự",
+                minlength: "Tên quá ngắn"
+            },
+            password: {
+                required: "Bắt buộc nhập password",
+                minlength: "Hãy nhập ít nhất 5 ký tự"
+            }
+        }
+    });
+    $("#frmRegister").validate({
+        rules: {
+            newUsername: {
+                required: true,
+                maxlength: 10,
+                minlength: 5
+            },
+            newEmail: {
+                required: true,
+                email: true
+            },
+            newPassword: {
+                required: true,
+                minlength: 5
+            },
+            Repassword: {
+                required: true,
+                minlength: 5,
+                equalTo: "#newPassword"
+            }
+        },
+        messages: {
+            newUsername: {
+                required: "Không để trống",
+                maxlength: "Quá dài rồi"
+            },
+            newEmail: {
+                required: "không để trống",
+                email: "Email chưa đúng định dạng"
+            },
+            newPassword: {
+                required: "Không để trống",
+                minlength: "Mật khẩu của bạn quá ngắn"
+            },
+            Repassword: {
+                required: "Không để trống",
+                minlength: "Mật khẩu của bạn quá ngắn",
+                equalTo: "Mật khẩu của bạn không trùng khớp"
+            }
+        }
+    });
+});
